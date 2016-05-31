737ced30a8d3d016e8d7b5af083687ecd955d483
Matteo Giachino <matteo.giachino@prima.it>
2016-05-31 10:26:21 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Posexional.Mixfile do
 
   def project do
     [app: :posexional,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
