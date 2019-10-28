c234f5064edd1312fb3f62d6ee7711f1e7b2f3ff
Tommaso Pifferi <tommaso.pifferi@prima.it>
2019-10-28 10:04:09 +0100

    Update to latest deps and include credo and dialyxir

--- mix.exs
+++ mix.exs
@@ -26,6 +26,18 @@ defmodule Posexional.Mixfile do
     [applications: [:logger]]
   end
 
+  defp description do
+    "A library to manage positional files"
+  end
+
+  defp package do
+    [
+      maintainers: ["Matteo Giachino"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/primait/posexional"}
+    ]
+  end
+
   # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
@@ -36,18 +48,23 @@ defmodule Posexional.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:credo, ">= 0.0.0", only: [:dev, :test]}, {:ex_doc, ">= 0.0.0", only: :dev}]
-  end
-
-  defp description do
-    "A library to manage positional files"
-  end
-
-  defp package do
     [
-      maintainers: ["Matteo Giachino"],
-      licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/primait/posexional"}
+      {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev},
+      {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      check: [
+        "format --check-formatted mix.exs \"lib/**/*.{ex,exs}\" \"test/**/*.{ex,exs}\" \"priv/**/*.{ex,exs}\" \"config/**/*.{ex,exs}\"",
+        "credo",
+        "dialyzer"
+      ],
+      "format.all": [
+        "format mix.exs \"lib/**/*.{ex,exs}\" \"test/**/*.{ex,exs}\" \"priv/**/*.{ex,exs}\" \"config/**/*.{ex,exs}\""
+      ]
     ]
   end
 end
